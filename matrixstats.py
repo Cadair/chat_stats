10db6a2d12058597402c9f90cb83cec4d62272d0
Stuart Mumford <stuart@cadair.com>
2019-04-05 17:59:03 +0100

    Add a notebook working on +matrix:matrix.org rooms

--- matrixstats.py
+++ matrixstats.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from matrix_client.errors import MatrixRequestError
 
 import pandas as pd
+import numpy as np
 
 
 __all__ = ['flatten_dicts', 'filter_events_by_messages', 'print_sorted_value',
@@ -15,7 +16,7 @@ __all__ = ['flatten_dicts', 'filter_events_by_messages', 'print_sorted_value',
            'get_all_messages_for_room', 'get_all_events']
 
 
-def get_all_messages_for_room(api, room_id):
+def get_all_messages_for_room(api, room_id, stop_time=None):
     """
     Use the matrix ``/messages`` API to back-paginate through the whole history
     of a room.
@@ -24,14 +25,27 @@ def get_all_messages_for_room(api, room_id):
     locally.
     """
     token = ""
-    token = api.get_room_messages(room_id, token, "b")['end']
     messages = []
+    try:
+        token = api.get_room_messages(room_id, token, "b")['end']
+    except MatrixRequestError:
+        print("Can't get messages for room...")
+        return messages
+
     for i in range(100):
         try:
             m1 = api.get_room_messages(room_id, token, "b", limit=5000)
         except MatrixRequestError:
             break
         token = m1['end']
+        # TODO: I am pretty sure this doesn't work
+        if stop_time:
+            stop_time = int(pd.Timestamp("2019/01/01").to_pydatetime().timestamp()*1000)
+            times = [e['origin_server_ts'] for e in m1['chunk']]
+            stopping = np.less(times, stop_time).nonzero()[0]
+            if len(stopping) > (len(times)/1.1):
+                messages += m1['chunk']
+                return messages
         messages += m1['chunk']
         if not m1['chunk']:
             break
@@ -59,7 +73,7 @@ def events_to_dataframe(list_o_json):
     return pd.DataFrame(df).set_index("origin_server_ts")
 
 
-def get_all_events(api, rooms, cache=None, refresh_cache=False):
+def get_all_events(api, rooms, cache=None, refresh_cache=False, stop_time=None):
     """
     Get all events in rooms.
 
@@ -73,7 +87,7 @@ def get_all_events(api, rooms, cache=None, refresh_cache=False):
         missing_keys = rooms.keys() - cache.keys()
         for key in missing_keys:
             print(f"fetching events for {key}")
-            cache[key] = events_to_dataframe(get_all_messages_for_room(api, rooms[key]))
+            cache[key] = events_to_dataframe(get_all_messages_for_room(api, rooms[key], stop_time=stop_time))
             store[key] = cache[key]
         for key in cache.keys() - rooms.keys():
             cache.pop(key)
@@ -83,7 +97,7 @@ def get_all_events(api, rooms, cache=None, refresh_cache=False):
         messages = {}
         for key, id in rooms.items():
             print(f"fetching events for {key}")
-            messages[key] = events_to_dataframe(get_all_messages_for_room(api, id))
+            messages[key] = events_to_dataframe(get_all_messages_for_room(api, id, stop_time=stop_time))
         if refresh_cache:
             with pd.HDFStore(cache) as store:
                 for channel, df in messages.items():
@@ -108,6 +122,18 @@ def get_rooms_in_community(api, communtiy):
     return ids
 
 
+def get_room_aliases_in_community(api, community):
+    rooms = api._send("GET", "/groups/{}/rooms".format(quote(community)))
+    ids = {}
+    for room in rooms['chunk']:
+        ca = room.get('canonical_alias')
+        if not ca:
+            continue
+        name = ca.split(":")[0][1:]
+        name = name.replace("-", "_")
+        ids[name] = ca
+    return ids
+
 def print_sorted_len(adict, reverse=True):
     for k in sorted(adict, key=lambda k: len(adict[k]), reverse=reverse):
         m = adict[k]
@@ -126,7 +152,7 @@ def filter_events_by_messages(events):
 
     events should be a dict of room events as returned by ``get_all_events``.
     """
-
+    
     messages = {k: v[v['type'] == "m.room.message"] for k, v in events.items()}
     messages = {k: v[v['sender'] != "@_neb_github_=40_cadair=3amatrix.org:matrix.org"] for k, v in messages.items()}
     return messages
